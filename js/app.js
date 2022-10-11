a2e7bde2687194c7320a539e297b3145108c1b6b
Vanne Vega <manelp3214@gmail.com>
2022-10-11 10:08:16 -0500

    fetch

--- js/app.js
+++ js/app.js
@@ -4,3 +4,7 @@
 if ( navigator.serviceWorker ) {
     navigator.serviceWorker.register('/sw.js');
 }
+
+fetch('https://reques.in/api/users')
+.then(resp => resp.text())
+.then(console.log);
--- sw.js
+++ sw.js
@@ -14,8 +14,17 @@ self.addEventListener('install', event => {
         console.log('SW: Instalaciones terminadas');
         self.skipWaiting();
         resolve();
-    }, 1000);
+    }, 1);
 });
     event.waitUntil(instalacion);
 
+});
+
+//FETCH
+self.addEventListener('fetch', event => {
+    //Aplicar estrategias del cache
+     console.log('SW:', event.request.url);
+         if (event.request.url.includes('https://reqres.in/')) {
+             const resp = new Response(`{ok: false, mensaje: 'jajaja'}`);
+              event.respondWith(resp); }
 });
